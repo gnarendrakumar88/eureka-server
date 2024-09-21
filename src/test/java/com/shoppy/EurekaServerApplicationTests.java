e609ce43f8c8d0d5f59e25436aa84439ace6fb91
gnarendrakumar88 <gnarendrakumar88@gmail.com>
2024-09-21 12:35:59 +0530

    Inital commit to add folders to git

---
+++ src/test/java/com/shoppy/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
